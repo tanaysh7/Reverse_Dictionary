73edbdabdfdcd9d766a50cad28458f233480c8fa
Vishal Seshagiri <vishalse@usc.edu>
2018-04-17 18:30:02 -0700

    convert all inputs to small case

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -211,7 +211,7 @@ class ReverseDictionaryForm extends Component {
 
 	dictionaryResult = (event) => {
 		var me = this;
-		var word= document.getElementById("wname").value;
+		var word= document.getElementById("wname").value.toLowerCase();
 		// var value = document.getElementById("result");
 
 		var dbRef = db.getValues('dictionary/', word)
@@ -231,7 +231,7 @@ class ReverseDictionaryForm extends Component {
 
 	reverseResult = (event) => {
 
-		var str= document.getElementById("rname").value;
+		var str= document.getElementById("rname").value.toLowerCase();
 		// english stop words
 		let ensw = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]	
 		str = str.split(" ").filter((e) => {return !ensw.includes(e)})
